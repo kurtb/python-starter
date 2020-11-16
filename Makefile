5f6242585b82c51a10db881b88bfeeb5889a85a3
Kurt Berglund <kurtberglund@hotmail.com>
2020-11-16 12:33:06 -0800

    Starter files

---
+++ Makefile
@@ -0,0 +1,54 @@
+# Inspiration from
+# https://blog.byronjsmith.com/makefile-shortcuts.html
+# https://github.com/edx/xblock
+
+.PHONY: jupyter requirements upgrade clean
+
+define PROJECT_HELP_MSG
+
+Usage:
+    make help           show help message
+    make $(VENV)           creates a new virtual environment
+
+    make clean          cleans up environemnt
+
+    make requirements   installs dev requirements
+    make upgrade        upgrades packages and regenerates all requirements files
+
+    make jupyter        starts Jupyter notebook server from current venv
+
+endef
+export PROJECT_HELP_MSG
+
+VENV = .env
+
+# set the venv path for commands to not require sourcing the env
+export VIRTUAL_ENV := $(abspath ${VENV})
+export PATH := ${VIRTUAL_ENV}/bin:${PATH}
+
+help:
+	@echo "$$PROJECT_HELP_MSG"
+
+$(VENV):
+	python3 -m venv $@
+	. $(VENV)/bin/activate; pip install -U pip wheel
+
+requirements: # Install requirements
+	pip install -r ./requirements/dev.txt
+	pip install -e .
+
+jupyter-setup:
+	jupyter nbextension enable --py widgetsnbextension
+	jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
+jupyter:
+	jupyter lab --ip=0.0.0.0 --port=8080
+
+upgrade: export CUSTOM_COMPILE_COMMAND=make upgrade
+upgrade:
+	# Install pip-tools first so that the subsequent commands can work
+	pip install -r requirements/pip-tools.txt
+	# Then upgrade packages. Order matters with the below. They should be upgraded in the order of dependencies.
+	pip-compile --upgrade requirements/pip-tools.in
+	pip-compile --upgrade setup.py -o requirements/requirements.txt
+	pip-compile --upgrade requirements/dev.in
---
+++ setup.py
@@ -0,0 +1,120 @@
+# Adapted from https://github.com/pypa/sampleproject/blob/master/setup.py
+# https://packaging.python.org/guides/distributing-packages-using-setuptools/#configuring-your-project
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='python-starter',
+    version='0.0.0',
+    description='Python starter kit',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kurtb/python-starter',
+    author='Kurt Berglund',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+
+    keywords='',  # Optional
+
+    # When your source code is in a subdirectory under the project root, e.g.
+    # `src/`, it is necessary to specify the `package_dir` argument.
+    # package_dir={'': 'src'},  # Optional
+
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(where='.'),  # Required
+
+    # Specify which Python versions you support. In contrast to the
+    # 'Programming Language' classifiers above, 'pip install' will check this
+    # and refuse to install the project if the version does not match. See
+    # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
+    python_requires='>=3.6, <4',
+
+    # This field lists other packages that your project depends on to run.
+    # Any package you put here will be installed by pip when your project is
+    # installed, so they must be valid existing projects.
+    #
+    # For an analysis of "install_requires" vs pip's requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "tqdm",
+    ],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). Users will be able to install these using the "extras"
+    # syntax, for example:
+    #
+    #   $ pip install sampleproject[dev]
+    #
+    # Similar to `install_requires` above, these must be valid existing
+    # projects.
+    # Optional
+    extras_require={
+        # consider making the cuda support optional. Especially once
+        # pip-compile deals with
+        # https://github.com/jazzband/pip-tools/issues/908 
+        # 'gpu': ['spacy[cuda101]'],
+    },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.
+    # package_data={  # Optional
+    #     'sample': ['package_data.dat'],
+    # },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/distutils/setupscript.html#installing-additional-files
+    #
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    # data_files=[('my_data', ['data/data_file'])],  # Optional
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    # entry_points={  # Optional
+    #     'console_scripts': [
+    #         'sample=sample:main',
+    #     ],
+    # },
+
+    # List additional URLs that are relevant to your project as a dict.
+    #
+    # This field corresponds to the "Project-URL" metadata fields:
+    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
+    #
+    # Examples listed include a pattern for specifying where the package tracks
+    # issues, where the source is hosted, where to say thanks to the package
+    # maintainers, and where to support the project financially. The key is
+    # what's used to render the link text on PyPI.
+    # project_urls={  # Optional
+    #     'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
+    #     'Funding': 'https://donate.pypi.org',
+    #     'Say Thanks!': 'http://saythanks.io/to/example',
+    #     'Source': 'https://github.com/pypa/sampleproject/',
+    # },
+)
